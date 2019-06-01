575f056526aba890bdd8493bfdbd3b36d44e379d
ThinGoods <vashchenko131@gmail.com>
2019-06-02 01:34:17 +0300

    accordion for news block

--- index.php
+++ index.php
@@ -16,35 +16,50 @@
 
 	$answer = $mysqli->query("SELECT * FROM `news` ORDER BY `date` DESC");
 	while(($row = $answer->fetch_assoc()) != false ){ $data[] = $row; }
- 
     foreach($data as $key=>$value){
-    	foreach($value as $keyData => $valueData) {
-    		$currentData[] = $valueData;
-    	}
+    	foreach($value as $keyData => $valueData) { $currentData[] = $valueData; }
 	}     
 
 	for($i = 0; $i < count($currentData); $i++){
-	  if( ($i % 5) == 0){
+	  if( ($i % 6) == 0){
 	  	echo "
 	  	  <div class='news'>
 	  	  	<div>title = ".$currentData[$i+1]."</div>
-	  	  	<div>short_desc = ".$currentData[$i+2]."</div>
-	  	  	<div>long_desc = ".$currentData[$i+3]."</div>
+	  	  	<img src= ".$currentData[$i+5].">
+	  	  	<div class='short_description'>short_desc = ".mb_strimwidth($currentData[$i+3], 0, 200, '.....')."</div>
+	  	  	<div class='long_description'>long_desc = ".$currentData[$i+3]."</div>
+	  	  	<div class='btn_expand'>+</div>
+	  	  	<div class='btn_unexpand'>-</div>
 	  	  	<div>date = ".$currentData[$i+4]."</div>
 	  	  </div>
 	  	  ";
 	  }
 	}
 
-	/*
-		----------------------------
-		ВЫВОД НОВОСТЕЙ С БАЗЫ ДАННЫХ
-		----------------------------
-	*/
-
-
-
-
+	/*-----------------------------
+	   ВЫВОД НОВОСТЕЙ С БАЗЫ ДАННЫХ
+	-----------------------------*/
 ?>
+
+<script type="text/javascript">
+const itemsOfShortDesc = document.getElementsByClassName('short_description');
+const itemsOfLongDesc = document.getElementsByClassName('long_description');
+const itemsOfButtons = document.getElementsByClassName('btn_expand');
+const itemsOfHideButtons = document.getElementsByClassName('btn_unexpand');
+
+function iteration(buttons, displayFirst, displaySecond) {
+  Array.prototype.forEach.call(buttons, (button, index) => {
+    button.addEventListener('click', () => { 
+  	  itemsOfShortDesc[index].style.display = displayFirst;
+  	  itemsOfLongDesc[index].style.display = displaySecond;
+  	  itemsOfHideButtons[index].style.display = displaySecond;
+  	  itemsOfButtons[index].style.display = displayFirst;
+    });
+  });
+};
+
+iteration(itemsOfButtons, 'none', 'block');
+iteration(itemsOfHideButtons, 'block', 'none');
+</script>
 </body>
 </html>
