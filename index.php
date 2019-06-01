3d90fe6e081470b3702f4caa1c212c96658d0d19
ThinGoods <vashchenko131@gmail.com>
2019-06-01 23:23:21 +0300

    sorting news

--- index.php
+++ index.php
@@ -6,6 +6,48 @@
 </head>
 <body>
 	<?php require("nav.php"); ?>
-MAIN STRANICA
+
+MAIN
+<?php 
+	echo "<br><br>";
+	require_once __DIR__."/function/connectMySQL/connect_to_db.php";
+	global $mysqli;
+	connectDB();
+
+	echo "<br><br>";
+
+	$answer = $mysqli->query("SELECT * FROM `news` ORDER BY `date` DESC");
+	while(($row = $answer->fetch_assoc()) != false ){ $data[] = $row; }
+ 
+    foreach($data as $key=>$value){
+    	foreach($value as $keyData => $valueData) {
+    		$currentData[] = $valueData;
+    	}
+	}     
+
+
+	for($i = 0; $i < count($currentData); $i++){
+	  if( ($i % 5) == 0){
+	  	echo "
+	  	  <div>
+	  	  	<div>title = ".$currentData[$i+1]."
+	  	  	<div>short_desc = ".$currentData[$i+2]."
+	  	  	<div>long_desc = ".$currentData[$i+3]."
+	  	  	<div>date = ".$currentData[$i+4]."
+	  	  </div>
+	  	  ";
+	  }
+	}
+
+	/*
+		----------------------------
+		ВЫВОД НОВОСТЕЙ С БАЗЫ ДАННЫХ
+		----------------------------
+	*/
+
+
+
+
+?>
 </body>
 </html>
--- nav.php
+++ nav.php
@@ -1,8 +1,12 @@
 <div> 
-
  	<a href="index.php"><div class="menu_item"> Головна </div></a>
- 	<div class="menu_item"> Про филию</div>
- 	<div class="menu_item"> Структура филии</div>
- 	<div class="menu_item"> Наука</div>
+ 	<div class=""> Про филию
+ 		<a href="#"><div class="menu_item"> - Історична довідка</div></a>
+ 		<a href="#"><div class="menu_item"> - Керівництво</div></a>
+ 		<a href="#"><div class="menu_item"> - Вчена рада</div></a>
+ 		<a href="#"><div class="menu_item"> - Контакти</div></a>
+ 		<a href="#"><div class="menu_item"> - Державні закупівлі</div></a>
+ 	</div>
+ 	<a href="#"><div class="menu_item"> Структура филии</div></a>
 
 </div>
